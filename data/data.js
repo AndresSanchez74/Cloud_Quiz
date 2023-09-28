cdad807f0951379bdb9855509aeb978480242e80
Andres <sanchezandreshc@gmail.com>
2023-09-28 11:42:40 -0500

    primer commit

---
+++ data/data.js
@@ -0,0 +1,61 @@
+export const data = [
+
+    {
+        question: '¿Qué son los 12 factores de una aplicación Cloud native?',
+        choices: [
+        'Principios para SaaS',
+        'Requisitos de diseño',
+        'Herramientas para desarrollo',
+        'Patrones de desarrollo'
+        ],
+        answer: 'Principios para SaaS',
+    },
+
+    {
+        question: '¿Qué es un Service Mesh y qué beneficios aporta a las aplicaciones Cloud native?',
+        choices: [
+        'Capa de infraestructura para servicios de red',
+        'Capa de aplicación para servicios de negocio',
+        'Capa de presentación para servicios de interfaz',
+        'Capa de datos para servicios de persistencia'
+        ],
+        answer: 'Capa de infraestructura para servicios de red'
+    },
+
+    {
+        question: 'Ventajas de usar contenedores en Cloud native:',
+        choices: [
+        'Empaquetamiento y aislamiento',
+        'Optimización de recursos',
+        'Automatización del ciclo de vida',
+        'Todas las anteriores'
+        ],
+        answer: 'Todas las anteriores'
+    },
+
+    {
+        question: '¿Qué es un microservicio y qué beneficios aporta a las aplicaciones Cloud native?',
+        choices: [
+        'Unidad de software que implementa una funcionalidad específica, despliegue independiente, comunicación mediante interfaces definidas',
+        'Unidad de software genérica, despliegue compartido, comunicación mediante mecanismos estándar',
+        'Unidad de software compleja, despliegue coordinado, comunicación mediante protocolos propietarios',
+        'Unidad de software simple, despliegue aislado, comunicación mediante mensajes directos'
+        ],
+        answer: 'Unidad de software que implementa una funcionalidad específica, despliegue independiente, comunicación mediante interfaces definidas'
+    },
+
+    {
+        question: '¿Qué es Cloud native?',
+        choices: [
+        'Nube y aplicaciones nativas',
+        'Desarrollo en la nube con escalabilidad y portabilidad',
+        'Migración a la nube con adaptación',
+        ' Tecnologías nativas de la nube'
+        ],
+        answer: ' Desarrollo en la nube con escalabilidad y portabilidad'
+    },
+
+]
+
+
+   
